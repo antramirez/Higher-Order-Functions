b119a59153e18de9afbfde545affc506204a316a
Anthony Ramirez <ar4477@nyu.edu>
2018-09-20 17:35:37 -0400

    add gitignore and listed functions in hoffy.js

--- src/hoffy.js
+++ src/hoffy.js
@@ -1,1 +1,45 @@
 // hoffy.js
+
+function longestString(s1, s2, ...sN) {
+  
+}
+
+function maybe(fn) {
+
+}
+
+function filterWith(fn) {
+
+}
+
+function steppedForEach(arr, fn, step) {
+
+}
+
+function constrainDecorator(fn, min, max) {
+
+}
+
+function limitCallsDecorator(fn, n) {
+
+}
+
+function bundleArgs(fn, args1, ...argsn) {
+
+}
+
+function sequence(fn1, fn2, ...fnN) {
+
+}
+
+
+module.exports = {
+  longestString: longestString,
+  maybe: maybe,
+  filterWith: filterWith,
+  steppedForEach: steppedForEach,
+  constrainDecorator: constrainDecorator,
+  limitCallsDecorator: limitCallsDecorator,
+  bundleArgs: bundleArgs,
+  sequence: sequence
+}
